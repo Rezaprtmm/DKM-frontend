abc911324942ae5e9a3c45a2fc2f80789de9fef9
Rezrfm <reza.maulana@students.paramadina.ac.id>
2023-05-30 09:36:15 +0700

    coba masa gabisa sih

--- pages/api/saveData.js
+++ pages/api/saveData.js
@@ -15,6 +15,7 @@ const CREDENTIALS_PATH = path.join(process.cwd(), "./credentials.json")
 module.exports = async function handler(req, res) {
   try {
     const { name, valEmail, inst, role } = req.body
+    res.status(200).json({ message: "Lah ini bisa pler wkwkwkk" })
     const head = "DKM-REG"
     const min = 100000
     const max = 999999
